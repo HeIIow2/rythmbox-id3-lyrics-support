26ba4e790ff64197358e869c2a251c42ab54c6b7
lars <Lars.Noack@outlook.de>
2022-11-09 15:01:43 +0100

    added shell skript to update

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "commit message: "
+# shellcheck disable=SC2162
+read commit_message
+
+git add .
+git commit -am "$commit_message"
+git push
+
+cd ~/.local/share/rhythmbox/plugins/rythmbox-id3-lyrics-support || exit
+git pull
